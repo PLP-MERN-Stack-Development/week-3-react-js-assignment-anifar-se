5260bd5ffc8a398e0476102c08a0da8be366e4c3
anifar-se <80062683+anifar-se@users.noreply.github.com>
2025-07-16 00:59:24 +0300

    Create tasks.jsx

---
+++ pages/tasks.jsx
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+import TaskManager from '../components/TaskManager';
+
+export default function Tasks() {
+  return (
+    <Layout>
+      <TaskManager />
+    </Layout>
+  );
+}
