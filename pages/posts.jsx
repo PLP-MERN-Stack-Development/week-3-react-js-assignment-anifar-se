ceb66a9df6ff295de5260c116141485d281e22c5
anifar-se <80062683+anifar-se@users.noreply.github.com>
2025-07-16 01:00:10 +0300

    Create posts.jsx

---
+++ pages/posts.jsx
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+import DataFetcher from '../components/DataFetcher';
+
+export default function Posts() {
+  return (
+    <Layout>
+      <DataFetcher />
+    </Layout>
+  );
+}
